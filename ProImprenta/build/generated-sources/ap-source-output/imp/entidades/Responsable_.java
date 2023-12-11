bf8f9f96ab971589b4ca01be6d89dfb838995471
NanoDan29 <jowell29jm@gmail.com>
2023-12-11 00:54:34 -0500

    eliminacion archivos innesarios

--- ProImprenta/build/generated-sources/ap-source-output/imp/entidades/Responsable_.java
+++ ProImprenta/build/generated-sources/ap-source-output/imp/entidades/Responsable_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20131113-rNA", date="2021-03-26T15:24:59")
+@Generated(value="EclipseLink-2.5.2.v20131113-rNA", date="2023-08-16T16:20:34")
 @StaticMetamodel(Responsable.class)
 public class Responsable_ { 
 
